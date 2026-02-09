73bfcc345b730f9f9fc6268670b6250ef282b97c
Juan José Álvarez Ocampo <juanjosealvarezag@gmail.com>
2026-02-08 23:37:01 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Movie
+from .models import Movie
 
 def home(request):
     context = {
@@ -8,3 +10,11 @@ def home(request):
 
 def about(request):
     return render(request, 'movie/about.html')
+
+def movie_list(request):
+    query = request.GET.get('q')
+    if query:
+        movies = Movie.objects.filter(title__icontains=query)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'movie/movie_list.html', {'movies': movies})
