97fd57ed929e35ed9dddf7e14c9d83fdbafbc71c
Juan José Álvarez <juanjosealvarezag@gmail.com>
2026-05-07 10:09:06 -0500

    Update settings.py

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-^6pei6(hlx_7%j7e6*xt^s-40vi@dbzb(n!1@3&e)%79^qbix5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.81.40.111']
 
 
 # Application definition
